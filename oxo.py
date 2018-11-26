b6012e0f4cc480a39e20a29f0787d92a4d10f182
Jakob <jc220866@falmouth.ac.uk>
2018-11-26 13:38:22 +0000

    i mdone

--- oxo.py
+++ oxo.py
@@ -1,26 +1,65 @@
 class OxoBoard:
     def __init__(self):
         """ The initialiser. Initialise any fields you need here. """
-        raise NotImplementedError("TODO: implement __init__")
+        # Lists are simple to call and easily mutable.
+        self.board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     def get_square(self, x, y):
         """ Return 0, 1 or 2 depending on the contents of the specified square. """
-        raise NotImplementedError("TODO: implement get_square")
+        return self.board[y * 3 + x]
 
     def set_square(self, x, y, mark):
         """ If the specified square is currently empty (0), fill it with mark and return True.
             If the square is not empty, leave it as-is and return False. """
-        raise NotImplementedError("TODO: implement set_square")
+
+        if self.board[y * 3 + x] == 0:
+            self.board[y * 3 + x] = mark
+            return True
+        else:
+            return False
 
     def is_board_full(self):
         """ If there are still empty squares on the board, return False.
             If there are no empty squares, return True. """
-        raise NotImplementedError("TODO: implement is_board_full")
 
-    def get_winner(self):
+        if 0 in self.board:
+            return False
+        else:
+            return True
+
+    def get_winner(self, current_turn):
         """ If a player has three in a row, return 1 or 2 depending on which player.
             Otherwise, return 0. """
-        raise NotImplementedError("TODO: implement get_winner")
+
+        # Row - Top
+        # Row - Middle
+        # Row - Bottom
+        # Column - Left
+        # Column - Middle
+        # Column - Right
+        # Diagonal - TopLeft to BottomRight
+        # Diagonal - TopRight to BottomLeft
+        # b = board, t = current_turn
+
+        # a pointer is used to increase readability of get_winner()
+        b = self.board
+
+        # another pointer for readability of get_winner()
+        t = current_turn
+
+        if (b[0] == t and b[1] == t and b[2]) or \
+            (b[3] == t and b[4] == t and b[5]) or \
+            (b[6] == t and b[7] == t and b[8]) or \
+            (b[0] == t and b[3] == t and b[6]) or \
+            (b[1] == t and b[4] == t and b[7]) or \
+            (b[2] == t and b[5] == t and b[8]) or \
+            (b[0] == t and b[4] == t and b[8]) or \
+                (b[2] == t and b[4] == t and b[6]):
+
+            return current_turn
+
+        else:
+            return 0
 
     def show(self):
         """ Display the current board state in the terminal. You should not need to edit this. """
